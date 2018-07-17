f5d1d8ea55d343c0fbb9d5241f9864b6ea7af3e4
Tom Lavery <devpuppyinc@gmail.com>
2018-07-17 10:48:49 -0700

    * changed base map to use esri shaded relief

--- ExtremeHeatBase/js/LoadBaseMap.js
+++ ExtremeHeatBase/js/LoadBaseMap.js
@@ -5,6 +5,6 @@
 var map = L.map('map').setView([52.5, -124], 6);
 
 //the initial basemap is set to the ESRI 'Gray' basemap which does not have any labels by default
-var grayBase = L.esri.basemapLayer('Gray').addTo(map);
+var shadedReliefBase = L.esri.basemapLayer('ShadedRelief').addTo(map);
 //add the labels after the basemap is added so the labels show up on top
-var grayLabels = L.esri.basemapLayer('GrayLabels').addTo(map);
+var shadedRelief = L.esri.basemapLayer('ShadedReliefLabels').addTo(map);
